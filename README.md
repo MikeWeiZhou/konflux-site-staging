db76bfcfeefa4567d9fddf934d7899091bfc5012
Wei Yi Zhou <mikeweizhou@gmail.com>
2021-03-23 13:57:27 -0700

    Staging v0.1.0

--- README.md
+++
@@ -1,1 +0,0 @@
-# konflux-site-staging
--- js/pro.fae89c3b.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["pro"],{"52b9":function(e,t,n){"use strict";n.r(t);var o,r,a=n("0dfe"),p=n("6512"),s={name:"Pro",metaInfo:{title:"Pro Features"},extends:a["a"],mixins:[Object(p["a"])(["hero-alt","pro-features"])],props:{id:{type:String,default:"pro"}}},l=s,u=n("2877"),i=Object(u["a"])(l,o,r,!1,null,null,null);t["default"]=i.exports}}]);
-//# sourceMappingURL=pro.fae89c3b.js.map
